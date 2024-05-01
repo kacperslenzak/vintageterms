08c080023cefdb80c3631f6638870865efb6b55d
Kacper <kacperinos@gmail.com>
2024-05-01 20:59:17 +0100

    Hide additional information tab

--- inc/functions/woocommerce.php
+++ inc/functions/woocommerce.php
@@ -84,3 +84,18 @@ function jd_add_to_cart_fragment( $fragments ) {
 	return $fragments;
 
 }
+
+/**
+ * @snippet       Remove Additional Information Tab @ WooCommerce Single Product Page
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @testedwith    WooCommerce 8
+ * @community     https://businessbloomer.com/club/
+ */
+
+add_filter( 'woocommerce_product_tabs', 'bbloomer_remove_product_tabs', 9999 );
+
+function bbloomer_remove_product_tabs( $tabs ) {
+	unset( $tabs['additional_information'] );
+	return $tabs;
+}
